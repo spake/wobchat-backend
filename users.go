a7576ca438268fd76d3583c075e03036b3477542
Chybby <achybby@gmail.com>
2015-09-21 17:31:09 +1000

    Created the add friends endpoint and relevant tests

--- users.go
+++ users.go
@@ -3,6 +3,8 @@ package main
 import (
     "log"
     "net/http"
+    "encoding/json"
+    "errors"
 )
 
 /*
@@ -37,15 +39,33 @@ type PublicUser struct {
     Picture     string  `json:"picture"`
 }
 
-func (user *User) getFriends() []User {
+func (user *User) getFriends() Users {
     friends := []User{}
     db.Joins("inner join user_friends on friend_uid = uid").Where(&UserFriend{UserUid: user.Uid}).Find(&friends)
     return friends
 }
 
-func (user *User) addFriend(friend User) {
-    userFriend := UserFriend{UserUid: user.Uid,FriendUid: friend.Uid}
-    db.Create(&userFriend)
+func (user *User) addFriend(friend User) error {
+    if user.Uid != friend.Uid {
+        tx := db.Begin()
+
+        userFriend := UserFriend{UserUid: user.Uid,FriendUid: friend.Uid}
+        if err := tx.Create(&userFriend).Error; err != nil {
+            tx.Rollback()
+            return err
+        }
+
+        userFriend = UserFriend{UserUid: friend.Uid,FriendUid: user.Uid}
+        if err := tx.Create(&userFriend).Error; err != nil {
+            tx.Rollback()
+            return err
+        }
+
+        tx.Commit()
+        return nil
+    } else {
+        return errors.New("Cannot add yourself as a friend")
+    }
 }
 
 func (user *User) toPublic() PublicUser {
@@ -71,9 +91,12 @@ func (users *Users) toPublic() (publicUsers []PublicUser) {
 func getUserFromInfo(info GoogleInfo) (user User) {
     log.Printf("Getting user %v\n", info.ID)
 
+    log.Println(db)
+
     // check if user already exists
-    if err := db.Where("uid = ?", info.ID).First(&user).Error; err != nil {
+    if err := db.Where(&User{Uid: info.ID}).First(&user).Error; err != nil {
         // create user
+        log.Println("Creating new user in db")
         user = User{
             Uid:        info.ID,
             Name:       info.DisplayName,
@@ -84,6 +107,7 @@ func getUserFromInfo(info GoogleInfo) (user User) {
         }
         db.Create(&user)
     } else {
+        log.Println("Updating existing user in db")
         // update things from the info, in case they've changed
         user.Uid = info.ID
         user.Name = info.DisplayName
@@ -113,19 +137,46 @@ func getCurrentUser(r *http.Request) (user User, ok bool) {
  */
 
 /*
- * /friends
+ * /friends endpoint
+ */
+
+func friendsHandler(w http.ResponseWriter, r *http.Request) int {
+    log.Println("Handling /friends")
+    user, ok := getCurrentUser(r)
+    if !ok {
+        return http.StatusUnauthorized
+    }
+
+    var resp interface{}
+
+    switch r.Method {
+    case "GET":
+        resp = listFriendsEndpoint(user)
+    case "POST":
+        decoder := json.NewDecoder(r.Body)
+        var req AddFriendsRequest
+        err := decoder.Decode(&req)
+        if err != nil {
+            return http.StatusBadRequest
+        }
+        resp = addFriendEndpoint(user, req)
+    default:
+        return http.StatusMethodNotAllowed
+    }
+
+    sendJSONResponse(w, resp)
+    return http.StatusOK
+}
+
+/*
+ * GET /friends
  * Gets a list of the current user's friends.
  */
 type ListFriendsResponse struct {
     Friends []PublicUser    `json:"friends"`
 }
 
-func listFriendsHandler(w http.ResponseWriter, r *http.Request) int {
-    user, ok := getCurrentUser(r)
-    if !ok {
-        return http.StatusUnauthorized
-    }
-
+func listFriendsEndpoint(user User) ListFriendsResponse {
     var friends Users
     friends = user.getFriends()
 
@@ -133,6 +184,47 @@ func listFriendsHandler(w http.ResponseWriter, r *http.Request) int {
         Friends: friends.toPublic(),
     }
 
-    sendJSONResponse(w, resp)
-    return http.StatusOK
+    return resp
+}
+
+/*
+ * POST /friends
+ * Adds a friend to the current user.
+ */
+type AddFriendsRequest struct {
+    Uid     string    `json:"uid"`
+}
+
+type AddFriendsResponse struct {
+    Success bool      `json:"success"`
+    Error   string    `json:"error"`
+}
+
+func addFriendEndpoint(user User, req AddFriendsRequest) AddFriendsResponse {
+    var friend User
+    dbErr := db.Where(&User{Uid: req.Uid}).First(&friend).Error
+
+    var resp AddFriendsResponse
+
+    log.Println("DB Error: ", dbErr)
+
+    if dbErr == nil {
+        addErr := user.addFriend(friend)
+
+        if addErr != nil {
+            resp = AddFriendsResponse{
+                Success: false,
+                Error:   addErr.Error()}
+        } else {
+            resp = AddFriendsResponse{
+                Success: true,
+                Error:   ""}
+        }
+    } else {
+        // friend they are trying to add not found
+        resp = AddFriendsResponse{
+                Success: false,
+                Error:   "Friend not found"}
+    }
+    return resp
 }
